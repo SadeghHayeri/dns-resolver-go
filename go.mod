c87f6c7d949b51225f23feae76be05916bc604c6
Sadegh Hayeri <hayerisadegh@gmail.com>
2022-05-31 15:24:30 +0430

    feat: Add resolver main functions

---
+++ go.mod
@@ -0,0 +1,8 @@
+module dns-resolver
+
+go 1.18
+
+require (
+	github.com/google/gopacket v1.1.19
+	github.com/wpalmer/gozone v0.0.0-20190618201257-3b1011627457
+)
---
+++ main.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/wpalmer/gozone"
+	"net"
+	"os"
+	"reflect"
+	"strings"
+)
+
+var MaxMessageSize int = 1000
+var DefaultTTL uint32 = 100
+var Hosts = map[string][]string{}
+var WorkerCount = 5
+var WorkerQueueSize = 10
+
+type QueueMessage struct {
+	conn *net.UDPConn
+	addr *net.UDPAddr
+	data []byte
+}
+
+//  DNSResourceRecord
+//  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                                               |
+//  /                                               /
+//  /                      NAME                     /
+//  |                                               |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                      TYPE                     |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                     CLASS                     |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                      TTL                      |
+//  |                                               |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                   RDLENGTH                    |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--|
+//  /                     RDATA                     /
+//  /                                               /
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+func getDnsAnswer(ip string, question string) layers.DNSResourceRecord {
+	return layers.DNSResourceRecord{
+		Name:  []byte(question),
+		Type:  layers.DNSTypeA,
+		Class: layers.DNSClassIN,
+		TTL:   DefaultTTL,
+		IP:    net.ParseIP(ip),
+	}
+}
+
+// DNS is specified in RFC 1034 / RFC 1035
+// +---------------------+
+// |        Header       |
+// +---------------------+
+// |       Question      | the question for the name server
+// +---------------------+
+// |        Answer       | RRs answering the question
+// +---------------------+
+// |      Authority      | RRs pointing toward an authority
+// +---------------------+
+// |      Additional     | RRs holding additional information
+// +---------------------+
+//
+//  DNS Header
+//  0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                      ID                       |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |QR|   Opcode  |AA|TC|RD|RA|   Z    |   RCODE   |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                    QDCOUNT                    |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                    ANCOUNT                    |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                    NSCOUNT                    |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+//  |                    ARCOUNT                    |
+//  +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+func getDnsResultPacket(question layers.DNSQuestion, answerIps []string, id uint16) layers.DNS {
+	var answers []layers.DNSResourceRecord
+	for _, ip := range answerIps {
+		answers = append(answers, getDnsAnswer(ip, string(question.Name)))
+	}
+
+	var responseCode layers.DNSResponseCode
+	if len(answers) != 0 {
+		responseCode = layers.DNSResponseCodeNoErr
+	} else {
+		responseCode = layers.DNSResponseCodeNXDomain
+	}
+
+	dnsResult := layers.DNS{
+		ID:           id,
+		ResponseCode: responseCode,
+
+		QR:     true,
+		OpCode: 0,
+		AA:     false,
+		TC:     false,
+		RD:     false,
+		RA:     false,
+		Z:      0,
+
+		QDCount: 1,
+		ANCount: uint16(len(answers)),
+		NSCount: 1,
+		ARCount: 0,
+
+		Questions: []layers.DNSQuestion{question},
+		Answers:   answers,
+	}
+	return dnsResult
+}
+
+func sendUDPPacket(conn *net.UDPConn, addr *net.UDPAddr, packet layers.DNS) {
+	buf := gopacket.NewSerializeBuffer()
+	packet.SerializeTo(buf, gopacket.SerializeOptions{})
+	conn.WriteToUDP(buf.Bytes(), addr)
+}
+
+func resolve(conn *net.UDPConn, addr *net.UDPAddr, data []byte) {
+	packet := gopacket.NewPacket(data, layers.LayerTypeDNS, gopacket.Default)
+	if dnsLayer := packet.Layer(layers.LayerTypeDNS); dnsLayer != nil {
+		dns, _ := dnsLayer.(*layers.DNS)
+		question := dns.Questions[0]
+		if question.Type == layers.DNSTypeA {
+			hostname := string(question.Name)
+			resultIps, _ := Hosts[getDnsKey(question.Type.String(), hostname)]
+			dnsResultPacket := getDnsResultPacket(question, resultIps, dns.ID)
+			fmt.Println("DNS Resolved", string(question.Name), resultIps)
+			sendUDPPacket(conn, addr, dnsResultPacket)
+		}
+	}
+}
+
+func getDnsKey(zoneType string, domain string) string {
+	return zoneType + "::" + strings.TrimSuffix(domain, ".")
+}
+
+func loadZoneFile(filePath string) {
+	stream, _ := os.Open(filePath)
+	var record gozone.Record
+	scanner := gozone.NewScanner(stream)
+
+	for {
+		err := scanner.Next(&record)
+		if err != nil {
+			break
+		}
+
+		if record.Type == gozone.RecordType_A {
+			dnsKey := getDnsKey(record.Type.String(), record.DomainName)
+			Hosts[dnsKey] = append(Hosts[dnsKey], record.Data[0])
+		}
+	}
+
+}
+
+func startResolverWorker(queue chan QueueMessage) {
+	for {
+		message := <-queue
+		resolve(message.conn, message.addr, message.data)
+	}
+}
+
+func sendToAny(ob QueueMessage, chs []chan QueueMessage) int {
+	var set []reflect.SelectCase
+	for _, ch := range chs {
+		set = append(set, reflect.SelectCase{
+			Dir:  reflect.SelectSend,
+			Chan: reflect.ValueOf(ch),
+			Send: reflect.ValueOf(ob),
+		})
+	}
+	to, _, _ := reflect.Select(set)
+	return to
+}
+
+func main() {
+	loadZoneFile("example.zone")
+	conn, err := net.ListenUDP("udp", &net.UDPAddr{
+		Port: 5353,
+		IP:   net.ParseIP("0.0.0.0"),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close()
+	fmt.Printf("server listening %s\n", conn.LocalAddr().String())
+
+	var workerQueues []chan QueueMessage
+	for i := 0; i < WorkerCount; i++ {
+		queue := make(chan QueueMessage)
+		workerQueues = append(workerQueues, queue)
+		go startResolverWorker(queue)
+	}
+
+	for {
+		message := make([]byte, MaxMessageSize)
+		rlen, from, err := conn.ReadFromUDP(message[:])
+		if err != nil {
+			panic(err)
+		}
+
+		queueMessage := QueueMessage{conn: conn, addr: from, data: message[:rlen]}
+		sendToAny(queueMessage, workerQueues)
+	}
+}
